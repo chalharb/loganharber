b5e5827f86be25b3e154048d401ff00e372c0338
Logan Harber <chalharb@gmail.com>
2023-04-26 18:55:53 -0500

    ci(devops): Updated deployment pipeline to work with vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://chalharb.github.io/loganharber/",
   test: {
     globals: true,
     environment: "jsdom",
